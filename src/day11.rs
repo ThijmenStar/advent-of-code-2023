95c3c05ef0b8d31ef8cd20c1071b2e2522ff97d7
Thijmen Star <39615534+ThijmenStar@users.noreply.github.com>
2023-12-11 10:35:21 +0100

    Complete day11 part2

--- src/day11.rs
+++ src/day11.rs
@@ -2,6 +2,8 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use glam::IVec2;
 use itertools::Itertools;
 
+const EXPANSION_FACTOR: i32 = 1_000_000;
+
 #[aoc_generator(day11)]
 pub fn parse_input(input: &str) -> Vec<IVec2> {
     input
@@ -17,8 +19,27 @@ pub fn parse_input(input: &str) -> Vec<IVec2> {
 }
 
 #[aoc(day11, part1)]
-pub fn solve_part1(universe: &[IVec2]) -> u32 {
-    // expand universe
+pub fn solve_part1(universe: &[IVec2]) -> u64 {
+    solve_for_factor(universe, 2)
+}
+
+
+#[aoc(day11, part2)]
+pub fn solve_part2(universe: &[IVec2]) -> u64 {
+    solve_for_factor(universe, EXPANSION_FACTOR)
+}
+
+fn solve_for_factor(universe: &[IVec2], factor: i32) -> u64 {
+    let exp_universe = expand_universe(universe, factor);
+
+    exp_universe
+        .iter()
+        .tuple_combinations()
+        .map(|(a, b)| manhattan_distance(a, b) as u64)
+        .sum()
+}
+
+fn expand_universe(universe: &[IVec2], factor: i32) -> Vec<IVec2> {
     let mut offset = IVec2::new(0, 0);
     let mut exp_universe: Vec<IVec2> = [IVec2::new(0, 0)]
         .iter()
@@ -26,7 +47,8 @@ pub fn solve_part1(universe: &[IVec2]) -> u32 {
         .tuple_windows()
         .map(|(a, b)| {
             if a.y != b.y {
-                offset.y += b.y - a.y - 1;
+                let dots = b.y - a.y - 1;
+                offset.y += dots * factor - dots;
             }
             *b + offset
         })
@@ -41,17 +63,13 @@ pub fn solve_part1(universe: &[IVec2]) -> u32 {
         .tuple_windows()
         .map(|(a, b)| {
             if a.x != b.x {
-                offset.x += b.x - a.x - 1;
+                let dots = b.x - a.x - 1;
+                offset.x += dots * factor - dots;
             }
             b + offset
         }).collect()
     ;
-
     exp_universe
-        .iter()
-        .tuple_combinations()
-        .map(|(a, b)| manhattan_distance(a, b))
-        .sum()
 }
 
 fn manhattan_distance(a: &IVec2, b: &IVec2) -> u32 {
@@ -73,20 +91,24 @@ mod test {
 .......#..
 #...#.....";
 
+    const SIMPLE_INPUT: &str = "#..#
+....
+#..#";
+
     #[test]
     fn parse_example() {
         assert_eq!(
             parse_input(EXAMPLE_INPUT),
             vec![
-                IVec2::new(4, 0),
-                IVec2::new(9, 1),
+                IVec2::new(3, 0),
+                IVec2::new(7, 1),
                 IVec2::new(0, 2),
-                IVec2::new(8, 5),
-                IVec2::new(1, 6),
-                IVec2::new(12, 7),
-                IVec2::new(9, 10),
-                IVec2::new(0, 11),
-                IVec2::new(5, 11),
+                IVec2::new(6, 4),
+                IVec2::new(1, 5),
+                IVec2::new(9, 6),
+                IVec2::new(7, 8),
+                IVec2::new(0, 9),
+                IVec2::new(4, 9),
             ]
         )
     }
@@ -94,5 +116,28 @@ mod test {
     #[test]
     fn solve_example_part1() {
         assert_eq!(solve_part1(&parse_input(EXAMPLE_INPUT)), 374)
+    }
+
+    #[test]
+    fn test_expansion() {
+        assert_eq!(expand_universe(&parse_input(SIMPLE_INPUT), 10),
+            vec![IVec2::new(0, 0), IVec2::new(0, 11), IVec2::new(21, 0), IVec2::new(21, 11)]
+        )
+    }
+
+    #[test]
+    fn solve_example_factor_10() {
+        assert_eq!(solve_for_factor(&parse_input(EXAMPLE_INPUT), 10), 1030)
+    }
+
+    #[test]
+    fn solve_example_factor_100() {
+        assert_eq!(solve_for_factor(&parse_input(EXAMPLE_INPUT), 100), 8410)
+    }
+
+
+    #[test]
+    fn solve_simple_factor_10() {
+        assert_eq!(solve_for_factor(&parse_input(SIMPLE_INPUT), 10), 11*2 + 21*2 + 32*2)
     }
 }
