98d6bb66134749df6b20b6c2b7d3e53c78fc32e5
Thijmen Star <39615534+ThijmenStar@users.noreply.github.com>
2023-12-16 10:57:04 +0100

    Complete day15 part2

--- src/day15.rs
+++ src/day15.rs
@@ -6,11 +6,48 @@ pub fn parse_input(input: &str) -> Vec<String> {
 }
 
 #[aoc(day15, part1)]
-pub fn solve_part1(input: &[String]) -> u32 {
-    input
+pub fn solve_part1(input: &[String]) -> usize {
+    input.iter().map(|s| hash(s)).sum()
+}
+
+#[aoc(day15, part2)]
+pub fn solve_part2(input: &[String]) -> usize {
+    let mut boxes: Vec<Vec<(&str, u8)>> = vec![vec![]; 256];
+
+    input.iter().for_each(|s| match s.chars().last() {
+        Some('-') => {
+            let label = &s[..s.len() - 1];
+            let index = hash(label);
+            boxes[index]
+                .iter()
+                .position(|(l, _)| &label == l)
+                .map(|pos| boxes[index].remove(pos));
+        }
+        _ => {
+            let (label, lens) = s.split_once('=').unwrap();
+            let index = hash(label);
+            let focal: u8 = lens.parse().unwrap();
+            boxes[index]
+                .iter()
+                .position(|(l, _)| &label == l)
+                .map(|pos| boxes[index][pos] = (label, focal))
+                .unwrap_or_else(|| boxes[index].push((label, focal)))
+        }
+    });
+
+    boxes
         .iter()
-        .map(|s| s.chars().fold(0, |val, c| ((val + c as u32) * 17) % 256))
+        .enumerate()
+        .flat_map(|(i, b)| {
+            b.iter()
+                .enumerate()
+                .map(move |(j, &(_, lens))| (i + 1) * (j + 1) * (lens as usize))
+        })
         .sum()
+}
+
+fn hash(s: &str) -> usize {
+    s.chars().fold(0, |val, c| ((val + c as usize) * 17) % 256)
 }
 
 #[cfg(test)]
@@ -32,5 +69,10 @@ mod tests {
     #[test]
     fn solve_example_part1() {
         assert_eq!(solve_part1(&parse_input(EXAMPLE_INPUT)), 1320)
+    }
+
+    #[test]
+    fn solve_example_part2() {
+        assert_eq!(solve_part2(&parse_input(EXAMPLE_INPUT)), 145)
     }
 }
